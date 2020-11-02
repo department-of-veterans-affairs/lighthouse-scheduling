2ef9eeff1717a536e8d1b67bb4bc26403e74ca1c
jhulbert <joshua.hulbert@libertyits.com>
2020-11-02 09:08:28 -0500

    Fix entrypoint

--- scheduling-tests/src/main/docker/entrypoint.sh
+++ scheduling-tests/src/main/docker/entrypoint.sh
@@ -6,7 +6,6 @@ if [ -z "${SENTINEL_BASE_DIR:-}" ]; then SENTINEL_BASE_DIR=/sentinel; fi
 cd $SENTINEL_BASE_DIR
 
 test -n "${K8S_ENVIRONMENT}"
-test -n "${CLIENT_KEY}"
 if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV=$K8S_ENVIRONMENT; fi
 if [ -z "${SCHEDULING_URL:-}" ]; then SCHEDULING_URL=https://$K8S_LOAD_BALANCER; fi
 
@@ -14,8 +13,8 @@ java-tests \
   --module-name "scheduling-tests" \
   --regression-test-pattern ".*IT\$" \
   --smoke-test-pattern ".*IT\$" \
-  -Dsentinel="$SENTINEL_ENV" \
-  -Dscheduling.url=$SCHEDULING_URL \
+  -Dsentinel="${SENTINEL_ENV}" \
+  -Dsentinel.scheduling.url="${SCHEDULING_URL}" \
   $@
 
 exit $?
