2167f3511cea4389e79d9e14d4a201188932fc6a
jhulbert <joshua.hulbert@libertyits.com>
2020-11-10 16:55:11 -0500

    Controller

--- scheduling/src/main/java/gov/va/api/lighthouse/scheduling/SchedulingController.java
+++ scheduling/src/main/java/gov/va/api/lighthouse/scheduling/SchedulingController.java
@@ -2,15 +2,14 @@ package gov.va.api.lighthouse.scheduling;
 
 import gov.va.api.health.r4.api.bundle.AbstractBundle;
 import gov.va.api.health.r4.api.bundle.BundleLink;
-import gov.va.api.health.r4.api.datatypes.Identifier;
 import gov.va.api.health.r4.api.elements.Reference;
 import gov.va.api.health.r4.api.resources.Appointment;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import lombok.Builder;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
     produces = {"application/json", "application/fhir+json"})
 public class SchedulingController {
 
+  private final String basepath;
+
+  SchedulingController(@Value("${scheduling.base-url:http://localhost:8060/}") String basepath) {
+    this.basepath = basepath;
+  }
+
   @SneakyThrows
   Appointment buildAppointment() {
     String appointmentID = "I2-5XYSWFRZ637QKNR6IIRKYHA5RY000109";
     String patientID = "1092387456V321456";
     String locationID = "I2-9QPSWFRZ530PLNR6IIRKYHA5RY031128";
     return Appointment.builder()
-        .identifier(Collections.singletonList(Identifier.builder().id(appointmentID).build()))
+        .resourceType("Appointment")
         .id(appointmentID)
         .status(Appointment.AppointmentStatus.booked)
         .participant(
@@ -37,7 +42,7 @@ public class SchedulingController {
                 Appointment.Participant.builder()
                     .actor(
                         Reference.builder()
-                            .reference("Patient/" + patientID)
+                            .reference(basepath + "r4/Patient/" + patientID)
                             .display(patientID)
                             .build())
                     .status(Appointment.ParticipationStatus.tentative)
@@ -45,7 +50,7 @@ public class SchedulingController {
                 Appointment.Participant.builder()
                     .actor(
                         Reference.builder()
-                            .reference("Location/" + locationID)
+                            .reference(basepath + "r4/Location/" + locationID)
                             .display(locationID)
                             .build())
                     .status(Appointment.ParticipationStatus.accepted)
@@ -60,65 +65,63 @@ public class SchedulingController {
   }
 
   /** Get appointment by search parameters. */
-  @GetMapping(value = "")
+  @GetMapping
   Appointment.Bundle readAppointmentSearchParameters(
-      @RequestParam(
-              value = "_id",
-              defaultValue = "I2-5XYSWFRZ637QKNR6IIRKYHA5RY000109",
-              required = false)
-          String id,
-      @RequestParam(value = "patient", defaultValue = "1092387456V321456", required = false)
-          String patient,
+      @RequestParam(value = "_id", required = false) String id,
+      @RequestParam(value = "patient", required = false) String patient,
       @RequestParam(value = "identifier", required = false) String identifier) {
     Appointment appointment = buildAppointment();
+    var queryString =
+        StubbedQueryStringBuilder.builder()
+            .id(Optional.ofNullable(id))
+            .identifier(Optional.ofNullable(identifier))
+            .pat(Optional.ofNullable(patient))
+            .build()
+            .toStubbedQueryString();
     return Appointment.Bundle.builder()
         .link(
             Arrays.asList(
                 BundleLink.builder()
                     .relation(BundleLink.LinkRelation.first)
-                    .url(
-                        "http://localhost:8060/r4/Appointment?identifier="
-                            + "I2-5XYSWFRZ637QKNR6IIRKYHA5RY000109"
-                            + "&page=1&count=1")
+                    .url(basepath + "r4/Appointment?" + queryString + "&page=1&count=1")
                     .build(),
                 BundleLink.builder()
                     .relation(BundleLink.LinkRelation.self)
-                    .url(
-                        "http://localhost:8060/r4/Appointment?identifier="
-                            + "I2-5XYSWFRZ637QKNR6IIRKYHA5RY000109"
-                            + "&page=1&count=1")
+                    .url(basepath + "r4/Appointment?" + queryString + "&page=1&count=1")
                     .build(),
                 BundleLink.builder()
                     .relation(BundleLink.LinkRelation.last)
-                    .url(
-                        "http://localhost:8060/r4/Appointment?identifier="
-                            + "I2-5XYSWFRZ637QKNR6IIRKYHA5RY000109"
-                            + "&page=1&count=1")
+                    .url(basepath + "r4/Appointment?" + queryString + "&page=1&count=1")
                     .build()))
         .resourceType("Bundle")
         .type(AbstractBundle.BundleType.searchset)
         .total(1)
-        .entry(List.of(Appointment.Entry.builder().resource(appointment).build()))
-        .id("93")
+        .entry(
+            List.of(
+                Appointment.Entry.builder()
+                    .fullUrl(basepath + "r4/Appointment/I2-5XYSWFRZ637QKNR6IIRKYHA5RY000109")
+                    .resource(appointment)
+                    .build()))
         .build();
   }
 
   @Builder
-  public class StubbedQueryStringBuilder {
+  public static class StubbedQueryStringBuilder {
     private Optional<String> id;
     private Optional<String> pat;
     private Optional<String> identifier;
 
     /** Builds a query string representation based on the query params provided. */
     public String toStubbedQueryString() {
-      StringBuilder queryString = new StringBuilder("Appointment");
-      id.ifPresent(s -> queryString.append("_id=").append(s).append("&"));
-      pat.ifPresent(s -> queryString.append("patient=").append(s).append("&"));
-      identifier.ifPresent(s -> queryString.append("identifier=").append(s).append("&"));
-      if (queryString.charAt(queryString.length() - 1) == '&') {
-        queryString.setLength(queryString.length() - 1);
+      StringBuilder queryStringBuilder = new StringBuilder();
+      id.ifPresent(s -> queryStringBuilder.append("_id=").append(s).append("&"));
+      pat.ifPresent(s -> queryStringBuilder.append("patient=").append(s).append("&"));
+      identifier.ifPresent(s -> queryStringBuilder.append("identifier=").append(s).append("&"));
+      var queryString = queryStringBuilder.toString();
+      if (queryString.length() > 1 && queryString.endsWith("&")) {
+        return queryString.substring(0, queryString.length() - 1);
       }
-      return queryString.toString();
+      return queryString;
     }
   }
 }
