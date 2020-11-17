625412c383b0fdaacc9e052f81ade3dfdaebc37c
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2020-11-17 14:42:51 -0500

    Sentinel grabs sentinel.*.api-path (#6)

--- scheduling-tests/src/test/java/gov/va/api/lighthouse/scheduling/tests/SearchIT.java
+++ scheduling-tests/src/test/java/gov/va/api/lighthouse/scheduling/tests/SearchIT.java
@@ -7,13 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import gov.va.api.health.r4.api.resources.Appointment;
 import gov.va.api.health.sentinel.ExpectedResponse;
 import gov.va.api.health.sentinel.TestClient;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class SearchIT {
 
   public void checkResponse(String endpoint, Class expectedClass) {
     TestClient btc = r4Scheduling();
     String apiPath = btc.service().urlWithApiPath();
+    log.info("Making request to: {}", apiPath + endpoint);
     ExpectedResponse response = btc.get(apiPath + endpoint);
     response.expect(200).expectValid(expectedClass);
     assertThat(response.response()).isNotNull();
