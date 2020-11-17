5cd02724ece0877cb8fc2d0c625a057d723cdca7
jhulbert <joshua.hulbert@libertyits.com>
2020-11-17 14:40:20 -0500

    Sentinel grabs sentinel.*.api-path

--- scheduling-tests/src/main/java/gov/va/api/lighthouse/scheduling/tests/SystemDefinitions.java
+++ scheduling-tests/src/main/java/gov/va/api/lighthouse/scheduling/tests/SystemDefinitions.java
@@ -39,7 +39,7 @@ class SystemDefinitions {
         .url(SentinelProperties.optionUrl(name, url))
         .port(port)
         .accessToken(() -> Optional.ofNullable(accessToken))
-        .apiPath(apiPath)
+        .apiPath(SentinelProperties.optionApiPath(name, apiPath))
         .build();
   }
 
