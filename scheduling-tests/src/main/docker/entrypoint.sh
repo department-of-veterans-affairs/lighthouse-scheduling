29455df373502dbb59e9bcaf4a8dbe6fde09591d
jhulbert <joshua.hulbert@libertyits.com>
2020-11-24 10:56:38 -0500

    Oauth IT

--- scheduling-tests/src/main/docker/entrypoint.sh
+++ scheduling-tests/src/main/docker/entrypoint.sh
@@ -2,29 +2,51 @@
 
 set -euo pipefail
 
+test -n "${K8S_ENVIRONMENT}"
+test -n "${K8S_LOAD_BALANCER}"
+
 if [ -z "${SENTINEL_BASE_DIR:-}" ]; then SENTINEL_BASE_DIR=/sentinel; fi
 cd $SENTINEL_BASE_DIR
+
+# =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
+
+main() {
+  if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV="${K8S_ENVIRONMENT}"; fi
+  if [ -z "${SCHEDULING_URL:-}" ]; then SCHEDULING_URL="https://${K8S_LOAD_BALANCER}"; fi
+
+  SYSTEM_PROPERTIES="-Dsentinel=${SENTINEL_ENV} -Dsentinel.scheduling.url=${SCHEDULING_URL}"
+
+  if [ -n "${SCHEDULING_API_PATH:-}" ]; then  addToSystemProperties "sentinel.scheduling.api-path" "${SCHEDULING_API_PATH}"; fi
+  if [ -n "${SCHEDULING_PORT:-}" ]; then addToSystemProperties "sentinel.scheduling.port" "${SCHEDULING_PORT}"; fi
+  if [ -n "${MAGIC_ACCESS_TOKEN:-}" ]; then addToSystemProperties "access-token" "${MAGIC_ACCESS_TOKEN}"; fi
+
+  # Skip adding these unless lab env
+  if [[ "${SENTINEL_ENV^^}" =~ .*LAB$ ]]; then setUpOauthTest; fi
+
+  java-tests \
+    --module-name "scheduling-tests" \
+    --regression-test-pattern ".*IT\$" \
+    --smoke-test-pattern ".*IT\$" \
+    $SYSTEM_PROPERTIES \
+    $@
+
+  exit $?
+}
+
+# =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
 
 addToSystemProperties() {
   SYSTEM_PROPERTIES+=" -D$1=$2"
 }
 
-test -n "${K8S_ENVIRONMENT}"
-test -n "${K8S_LOAD_BALANCER}"
+setUpOauthTest() {
+  if [ -n "${OAUTH_AUD:-}" ]; then addToSystemProperties "va-oauth-robot.aud" "${OAUTH_AUD}"; fi
+  if [ -n "${OAUTH_TOKEN_URL:-}" ]; then addToSystemProperties "va-oauth-robot.token-url" "${OAUTH_TOKEN_URL}"; fi
+  if [ -n "${OAUTH_SCOPES:-}" ]; then addToSystemProperties "va-oauth-robot.scopes-csv" "${OAUTH_SCOPES}"; fi
+  addToSystemProperties "va-oauth-robot.client-id" "${OAUTH_CLIENT_ID}"
+  addToSystemProperties "va-oauth-robot.client-secret" "${OAUTH_CLIENT_SECRET}"
+}
 
-if [ -z "${SENTINEL_ENV:-}" ]; then SENTINEL_ENV="${K8S_ENVIRONMENT}"; fi
-if [ -z "${SCHEDULING_URL:-}" ]; then SCHEDULING_URL="https://${K8S_LOAD_BALANCER}"; fi
+# =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=
 
-SYSTEM_PROPERTIES="-Dsentinel=${SENTINEL_ENV} -Dsentinel.scheduling.url=${SCHEDULING_URL}"
-
-if [ -n "${SCHEDULING_API_PATH:-}" ]; then  addToSystemProperties "sentinel.scheduling.api-path" "${SCHEDULING_API_PATH}"; fi
-if [ -n "${MAGIC_ACCESS_TOKEN:-}" ]; then addToSystemProperties "access-token" "${MAGIC_ACCESS_TOKEN}"; fi
-
-java-tests \
-  --module-name "scheduling-tests" \
-  --regression-test-pattern ".*IT\$" \
-  --smoke-test-pattern ".*IT\$" \
-  $SYSTEM_PROPERTIES \
-  $@
-
-exit $?
+main $@
---
+++ scheduling-tests/src/test/java/gov/va/api/lighthouse/scheduling/tests/OauthIT.java
@@ -0,0 +1,78 @@
+package gov.va.api.lighthouse.scheduling.tests;
+
+import static gov.va.api.health.sentinel.EnvironmentAssumptions.assumeEnvironmentIn;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import gov.va.api.health.r4.api.resources.Appointment;
+import gov.va.api.health.r4.api.resources.OperationOutcome;
+import gov.va.api.health.sentinel.Environment;
+import gov.va.api.health.sentinel.OauthRobotProperties;
+import gov.va.api.health.sentinel.SystemScopeOauthRobot;
+import gov.va.api.health.sentinel.TestClient;
+import gov.va.api.health.sentinel.TokenExchange;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class OauthIT {
+  TestClient client = TestClients.r4Scheduling();
+
+  TestIds ids = SystemDefinitions.systemDefinition().testIds();
+
+  private String exchangeToken() {
+    SystemScopeOauthRobot.Configuration config =
+        OauthRobotProperties.forSystemScopes()
+            // Loading ClientId, ClientSecret, Audience, and TokenUrl will be done via properties
+            .defaultScopes(List.of("system/Appointment.read"))
+            .build()
+            .oauthSystemScopeConfig();
+    TokenExchange token = SystemScopeOauthRobot.builder().config(config).build().token();
+    if (token.isError()) {
+      fail("Failed to get token result from oauth robot.");
+    }
+    return token.accessToken();
+  }
+
+  @Test
+  void oauthFlow() {
+    assumeEnvironmentIn(Environment.STAGING_LAB, Environment.LAB);
+    String token = exchangeToken();
+    // Valid Token
+    test(
+        200,
+        Appointment.Bundle.class,
+        token,
+        client.service().urlWithApiPath() + "Appointment?patient={icn}",
+        ids.oauthPatient());
+    // Invalid Token
+    test(
+        401,
+        OperationOutcome.class,
+        "NOPE",
+        client.service().urlWithApiPath() + "Appointment?patient={icn}",
+        ids.oauthPatient());
+    // Invalid Resource for Scopes
+    test(
+        200,
+        Appointment.Bundle.class,
+        token,
+        client.service().urlWithApiPath() + "Observation?patient={icn}",
+        ids.oauthPatient());
+  }
+
+  private void test(int status, Class<?> expected, String token, String path, String... params) {
+    log.info(
+        "OAUTH: Expect {} ({}) for {} {}",
+        expected.getName(),
+        status,
+        path,
+        Arrays.toString(params));
+    client
+        .get(Map.of("Authorization", "Bearer " + token), path, params)
+        .expect(status)
+        .expectValid(expected);
+  }
+}
